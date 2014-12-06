1c08d87d8918dc7e72ab0c436727bb2847ae32c0
JEexplanations <JEexplanations@Janes-Laptop.local>
2014-12-06 10:21:21 -0500

    Initial Commit

---
+++ Dino App FTW 2Tests/Dino_App_FTW_2Tests.swift
@@ -0,0 +1,36 @@
+//
+//  Dino_App_FTW_2Tests.swift
+//  Dino App FTW 2Tests
+//
+//  Created by JEexplanations on 12/6/14.
+//  Copyright (c) 2014 JEexplanations. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class Dino_App_FTW_2Tests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
